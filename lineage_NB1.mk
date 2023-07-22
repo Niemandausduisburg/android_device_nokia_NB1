06d09ddffeced4ecfefc7591395f850ae63602be
Niemandausduisburg <niemandausduisburg@gmx.net>
2023-07-22 11:02:34 +0200

    NB1: Build MindtheGapps For a unknown reason the delivered Gapps package doesn't work on NB1/NLA and RisingOS reaches a unbootable state. MinttheGapps build inline the ROM-Source and works fine with RisingOS. Add a build-flag into device-tree to include MindtheGapps.

--- lineage_NB1.mk
+++ lineage_NB1.mk
@@ -17,6 +17,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from NB1 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# MindtheGapps
+ifeq ($(BUILD_GAPPS),true)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+endif
+
 PRODUCT_NAME := lineage_NB1
 PRODUCT_DEVICE := NB1
 PRODUCT_MANUFACTURER := HMD Global
