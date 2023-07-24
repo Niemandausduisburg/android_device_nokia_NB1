777d5dc6738d5a8a6f204303b1b1a3b878d26c35
log1cs <tuan73176@gmail.com>
2023-07-25 03:55:05 +0700

    NLA: Build audio effects packages
    Also:
    * Uprev SoundTrigger to 2.2
    * Removes libshoebox since it's not used in audio_effects.xml
    
    Change-Id: I04530d50e29107dbc622e55d3db6c6d06c0f2eb6

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.primary.msm8998 \
@@ -34,6 +34,8 @@ PRODUCT_PACKAGES += \
     
 PRODUCT_PACKAGES += \
     libaudio-resampler \
+    libaudioroute \
+    libtinycompress \
     libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
